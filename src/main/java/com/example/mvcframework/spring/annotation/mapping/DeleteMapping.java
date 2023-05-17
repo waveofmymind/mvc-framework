dfcd014a56de77a1caf562d9acbd70a98bec21dd
waveofmymind <sjun0913@gmail.com>
2023-05-17 16:07:22 +0900

    [FEAT] 어노테이션용 폴더 추가 및 이동

--- src/main/java/com/example/mvcframework/spring/annotation/mapping/DeleteMapping.java
+++ src/main/java/com/example/mvcframework/spring/annotation/mapping/DeleteMapping.java
@@ -1,4 +1,4 @@
-package com.example.mvcframework.spring.annotation;
+package com.example.mvcframework.spring.annotation.mapping;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface GetMapping {
+public @interface DeleteMapping {
 
     String value() default "";
 }
