8cf47887631c734baf921620d0343ea1372bd555
waveofmymind <sjun0913@gmail.com>
2023-05-17 11:27:05 +0900

    [FEAT] 컨트롤러 구현시 사용할 예제 클래스

---
+++ src/main/java/com/example/mvcframework/db/ResultData.java
@@ -0,0 +1,11 @@
+package com.example.mvcframework.db;
+
+public class ResultData {
+
+    private String resultCode;
+    private String msg;
+    private Object data;
+
+    public ResultData(String resultCode, String msg, Object data) {
+    }
+}
