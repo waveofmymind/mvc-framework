f7107a54bafe8e458bae503e5fc0fa218f46adf8
waveofmymind <sjun0913@gmail.com>
2023-05-17 17:55:51 +0900

    [FIX] 매핑 메서드 별로 인식하도록 로직 변경

--- src/main/java/com/example/mvcframework/spring/ControllerManager.java
+++ src/main/java/com/example/mvcframework/spring/ControllerManager.java
@@ -1,8 +1,12 @@
 package com.example.mvcframework.spring;
 
+import com.example.mvcframework.db.DbConfig;
 import com.example.mvcframework.db.Rq;
 import com.example.mvcframework.spring.annotation.Controller;
+import com.example.mvcframework.spring.annotation.mapping.DeleteMapping;
 import com.example.mvcframework.spring.annotation.mapping.GetMapping;
+import com.example.mvcframework.spring.annotation.mapping.PostMapping;
+import com.example.mvcframework.spring.annotation.mapping.PutMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.reflections.Reflections;
@@ -13,32 +17,56 @@ import java.util.Map;
 import java.util.Set;
 
 public class ControllerManager {
-    private static Map<String, Method> getMappingMethods = new HashMap<>();
+    private static Map<String, Method> controllerMap = new HashMap<>();
 
     static {
-        Reflections reflections = new Reflections("com.example.mvcframework");
+        Reflections reflections = new Reflections(DbConfig.BASE_PACKAGE_PATH);
 
         Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
 
         for (Class<?> controller : controllers) {
             for (Method method : controller.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(GetMapping.class)) {
-                    GetMapping getMapping = method.getAnnotation(GetMapping.class);
-                    getMappingMethods.put(getMapping.value(), method);
-                }
+                registerMethod(method);
             }
+        }
+    }
+
+    private static void registerMethod(Method method) {
+        String methodType = null;
+        String mappingValue = null;
+        if (method.isAnnotationPresent(GetMapping.class)) {
+            methodType = "GET";
+            GetMapping getMapping = method.getAnnotation(GetMapping.class);
+            mappingValue = getMapping.value();
+        } else if (method.isAnnotationPresent(PostMapping.class)) {
+            methodType = "POST";
+            PostMapping postMapping = method.getAnnotation(PostMapping.class);
+            mappingValue = postMapping.value();
+        } else if (method.isAnnotationPresent(PutMapping.class)) {
+            methodType = "PUT";
+            PutMapping putMapping = method.getAnnotation(PutMapping.class);
+            mappingValue = putMapping.value();
+        } else if (method.isAnnotationPresent(DeleteMapping.class)) {
+            methodType = "DELETE";
+            DeleteMapping deleteMapping = method.getAnnotation(DeleteMapping.class);
+            mappingValue = deleteMapping.value();
+        }
+
+        if (methodType != null && mappingValue != null) {
+            controllerMap.put(methodType + ":" + mappingValue, method);
         }
     }
 
     public static void runAction(HttpServletRequest req, HttpServletResponse resp) {
         String requestURI = req.getRequestURI();
-        Method method = getMappingMethods.get(requestURI);
+        String requestMethod = req.getMethod();
+        Method method = controllerMap.get(requestMethod + ":" + requestURI);
 
         if (method != null) {
             try {
                 Object controller = Container.getObj(method.getDeclaringClass());
 
-                    method.invoke(controller, new Rq(req, resp));
+                method.invoke(controller, new Rq(req, resp));
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -47,4 +75,5 @@ public class ControllerManager {
         }
     }
 }
+
 
