238b95396cc1b0687fe84f8904f37996c71bb2b4
waveofmymind <sjun0913@gmail.com>
2023-05-18 09:12:19 +0900

    [FEAT] 컨트롤러 로직 추가

--- src/main/java/com/example/mvcframework/article/ArticleController.java
+++ src/main/java/com/example/mvcframework/article/ArticleController.java
@@ -3,7 +3,8 @@ package com.example.mvcframework.article;
 import com.example.mvcframework.db.Rq;
 import com.example.mvcframework.spring.annotation.Autowired;
 import com.example.mvcframework.spring.annotation.Controller;
-import com.example.mvcframework.spring.annotation.GetMapping;
+import com.example.mvcframework.spring.annotation.mapping.GetMapping;
+import com.example.mvcframework.spring.annotation.mapping.PostMapping;
 
 import java.util.List;
 
@@ -20,6 +21,64 @@ public class ArticleController {
         System.out.println("되나요");
         rq.setAttr("articles", articles);
         rq.view("usr/article/list"); // src/main/webapp/jsp/usr/article/list.jsp 실행
+    }
+
+    @GetMapping("/usr/article/write")
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+    }
+
+    @PostMapping("/usr/article/write")
+    public void write(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        if (title.length() == 0) {
+
+            rq.historyBack("제목을 입력해주세요.");
+            return;
+        }
+
+        if (body.length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+
+        long id = articleService.write(title, body);
+
+        rq.replace("/usr/article/%d".formatted(id), "%d번 게시물이 생성 되었습니다.".formatted(id));
+    }
+
+
+    @GetMapping("/usr/article/{id}")
+    public void showDetail(Rq rq) {
+
+        //TODO: @PathVariable 구현하기 또는 {id} 값 동적으로 받
+        long id = Long.parseLong(rq.getPathParam("id", "2"));
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        Article article = articleService.getArticleById(id);
+
+        if (article == null) {
+            rq.historyBack("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        Article prevArticle = articleService.getPrevArticle(article.getId());
+        Article nextArticle = articleService.getNextArticle(article.getId());
+
+        rq.setAttr("prevArticle", prevArticle);
+        rq.setAttr("nextArticle", nextArticle);
+
+        List<Article> articles = articleService.getArticles();
+
+        rq.setAttr("articles", articles);
+
+        rq.setAttr("article", article);
+        rq.view("usr/article/detail");
     }
 
 
